6cc1a56ec38b2a302893791fc919676dea5f7fdb
ivanliao99 <ivan321.liao@gmail.com>
2025-05-03 21:55:14 +0800

    Update CssForNginxExplorer.js

--- employee-service/CssForNginxExplorer.js
+++ employee-service/CssForNginxExplorer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Css for nginx explorer
 // @namespace    http://tampermonkey.net/
-// @version      2025-05-03
+// @version      2025-05-03-1
 // @description  try to take over the world!
 // @author       You
 // @match        http://192.168.1.103:7799/*
