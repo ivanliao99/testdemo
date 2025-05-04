f26956de259418f1b3e46397eb08740b7a9050e7
ivanliao99 <ivan321.liao@gmail.com>
2025-05-04 09:53:45 +0800

    Create  CloseNewTab.js

---
+++ employee-service/CloseNewTab.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Close new tab
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       You
+// @match        https://bisniscpm.asia/track.php/*
+// @match        https://buyproduct.my.id/*
+// @match        https://tardtodtimpi.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.close();
+
+})();
