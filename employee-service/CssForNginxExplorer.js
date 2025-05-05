5c58d29efc2d29cf0900f9e342ff968c3ab4a71c
ivanliao99 <ivan321.liao@gmail.com>
2025-05-05 22:24:36 +0800

    Update CssForNginxExplorer.js

--- employee-service/CssForNginxExplorer.js
+++ employee-service/CssForNginxExplorer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Css for nginx explorer
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.4.1
 // @description  try to take over the world!
 // @author       You
 // @match        http://192.168.1.103:7799/*
@@ -21,14 +21,14 @@
 
 
 		setTimeout(() => {
-			
+
 			document.querySelectorAll('a').forEach(originalLink => {
 				// 创建新的 a 标签
 				const newLink = document.createElement('a');
 				newLink.href = originalLink.href;
-				newLink.textContent = '🔗To new tab';
+				newLink.textContent = 'to new tab';
 				newLink.target = '_blank'; // 新标签页打开
-				newLink.style.marginLeft = '8px'; // 可选样式：添加一点间距
+				newLink.style.marginLeft = '3px'; // 可选样式：添加一点间距
 			  
 				// 插入到原始链接后面
 				originalLink.parentNode.insertBefore(newLink, originalLink.nextSibling);
