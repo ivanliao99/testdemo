d863702b1c5a27d7b015876354fff5648b43075b
ivanliao99 <ivan321.liao@gmail.com>
2025-05-05 22:16:51 +0800

    Update CssForNginxExplorer.js

--- employee-service/CssForNginxExplorer.js
+++ employee-service/CssForNginxExplorer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Css for nginx explorer
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        http://192.168.1.103:7799/*
@@ -20,31 +20,31 @@
 	if (isMobile()) {
 
 
-		setTimeout(() => {
-			document.querySelectorAll('a').forEach(link => {
-				let clickTimer = null;
+		// setTimeout(() => {
+		// 	document.querySelectorAll('a').forEach(link => {
+		// 		let clickTimer = null;
 
-				link.addEventListener('click', function (event) {
-					event.preventDefault();
+		// 		link.addEventListener('click', function (event) {
+		// 			event.preventDefault();
 
-					// 如果在短时间内点击两次，视为双击
-					if (clickTimer !== null) {
-						clearTimeout(clickTimer);
-						clickTimer = null;
-						// 双击：在新标签页打开链接
-						window.open(link.href, '_blank');
-					} else {
-						// 设置点击定时器，等待判断是否为双击
-						clickTimer = setTimeout(() => {
-							clickTimer = null;
-							// 这里是单击行为（如果你不想保留单击，就可以注释掉这行）
-							window.location.href = link.href;
-						}, 150); // 250ms 是常用的双击间隔判断时间
-					}
-				});
-			});
+		// 			// 如果在短时间内点击两次，视为双击
+		// 			if (clickTimer !== null) {
+		// 				clearTimeout(clickTimer);
+		// 				clickTimer = null;
+		// 				// 双击：在新标签页打开链接
+		// 				window.open(link.href, '_blank');
+		// 			} else {
+		// 				// 设置点击定时器，等待判断是否为双击
+		// 				clickTimer = setTimeout(() => {
+		// 					clickTimer = null;
+		// 					// 这里是单击行为（如果你不想保留单击，就可以注释掉这行）
+		// 					window.location.href = link.href;
+		// 				}, 150); // 250ms 是常用的双击间隔判断时间
+		// 			}
+		// 		});
+		// 	});
 
-		}, 500)
+		// }, 500)
 
 		// 创建一个 <style> 元素
 		const style = document.createElement('style');
